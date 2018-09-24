ebda4c2aad17ef912eca58fdbc8f219963853263
Daniel Pérez <danielp@giffgaff.co.uk>
2018-09-24 21:13:05 +0100

    Add creative style for cards

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './Card.css';
+
+const Card: React.StatelessComponent = () => (
+  <div className="card">
+    {/* <img className="card__img" src="https://picsum.photos/200/300/?random" /> */}
+  </div>
+);
+
+export default Card;
--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 
 import './Content.css';
+import Card from '../Card';
+
+const cards: React.ReactNode[] = [];
+
+for (let i = 0; i < 8; i++) {
+  cards.push(<Card key={i} />);
+}
 
 const Content: React.StatelessComponent = () => (
   <div className="content">
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est necessitatibus officiis eum obcaecati accusantium provident? Tempora in, praesentium aperiam autem repellendus corporis incidunt maiores reiciendis culpa cum odit at earum?</p>
+    {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est necessitatibus officiis eum obcaecati accusantium provident? Tempora in, praesentium aperiam autem repellendus corporis incidunt maiores reiciendis culpa cum odit at earum?</p> */}
+    {cards}
   </div>
 );
 
