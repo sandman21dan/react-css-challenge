cd0bf50fd07d632e16ebf2e0c255e540b328fbca
Daniel Pérez <danielp@giffgaff.co.uk>
2018-09-25 14:10:32 +0100

    Add close button for cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 
 import './Card.css';
+import CloseButton from '../CloseButton';
 
 const Card: React.StatelessComponent = () => (
   <div className="card">
     {/* <img className="card__img" src="https://picsum.photos/200/300/?random" /> */}
+    <div className="card__close">
+      <CloseButton />
+    </div>
   </div>
 );
 
---
+++ src/components/CloseButton/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import './CloseButton.css';
+
+const CloseButton: React.StatelessComponent = () => (
+  <span className="close-btn" role="button">X</span>
+);
+
+export default CloseButton;
