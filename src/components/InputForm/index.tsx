6c601a93f61b1aca298a37e8e69218255fcd1d2f
Daniel Pérez <danielp@giffgaff.co.uk>
2018-09-26 15:32:46 +0100

    Add delete style button

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -6,6 +6,7 @@ const InputForm: React.StatelessComponent = () => (
   <div className="input-form">
     <input className="input-form__input" type="text"/>
     <button className="input-form__btn">Add</button>
+    <button className="input-form__btn input-form__btn--delete">Delete</button>
   </div>
 );
 
